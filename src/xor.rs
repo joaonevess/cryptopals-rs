26dbc6f3f72c1810afd8c0275163b16c5a3bf880
joaonevess <joaocns0@protonmail.com>
2022-12-02 19:16:37 -0300

    s1c5

--- src/xor.rs
+++ src/xor.rs
@@ -1,3 +1,7 @@
+pub fn expand_key(key: &[u8], len: usize) -> Vec<u8> {
+    key.iter().cycle().take(len).copied().collect()
+}
+
 pub fn xor(a: &[u8], b: &[u8]) -> Result<Vec<u8>, &'static str> {
     // Make sure the slices are the same length
     let length = a.len();
@@ -9,6 +13,7 @@ pub fn xor(a: &[u8], b: &[u8]) -> Result<Vec<u8>, &'static str> {
     let result = a.iter().zip(b.iter()).map(|(x, y)| x ^ y).collect();
     Ok(result)
 }
+
 pub fn break_single_byte_xor(ciphertext: &[u8]) -> u8 {
     // Create a frequency array of the ciphertext
     let mut freq = [0; 256];
@@ -29,6 +34,18 @@ pub fn break_single_byte_xor(ciphertext: &[u8]) -> u8 {
     // The most common byte in English text is the space character
     // We can use this to find the key byte
     max_b ^ b' '
+}
+
+fn repeating_key_xor(input: &[u8], key: &[u8]) -> Result<Vec<u8>, &'static str> {
+    if key.is_empty() {
+        return Err("key cannot be empty");
+    }
+
+    Ok(input
+        .iter()
+        .zip(key.iter().cycle())
+        .map(|(x, y)| x ^ y)
+        .collect())
 }
 
 #[cfg(test)]
@@ -63,5 +80,16 @@ mod tests {
         let expected_key = 'X' as u8;
         let key = break_single_byte_xor(&ciphertext);
         assert_eq!(key, expected_key);
+    }
+
+    #[test]
+    fn test_repeating_key_xor() {
+        // Test repeating-key XOR
+        let input = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let key = b"ICE";
+        let expected_output =
+            hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap();
+        let output = repeating_key_xor(input, key).unwrap();
+        assert_eq!(output, expected_output);
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use cryptopals_rs;
+    use cryptopals_rs::*;
 
     #[test]
     fn set_1_challenge_1() {
@@ -16,7 +16,7 @@ mod tests {
     fn set_1_challenge_2() {
         let raw_bytes1 = hex::decode("1c0111001f010100061a024b53535009181c").unwrap();
         let raw_bytes2 = hex::decode("686974207468652062756c6c277320657965").unwrap();
-        let xor_bytes = cryptopals_rs::xor::xor(&raw_bytes1, &raw_bytes2).unwrap();
+        let xor_bytes = xor::xor(&raw_bytes1, &raw_bytes2).unwrap();
         let xor_str = hex::encode(&xor_bytes);
         assert_eq!(xor_str, "746865206b696420646f6e277420706c6179");
     }
@@ -28,9 +28,9 @@ mod tests {
             hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
                 .unwrap();
 
-        let key = cryptopals_rs::xor::break_single_byte_xor(&raw_bytes);
+        let key = xor::break_single_byte_xor(&raw_bytes);
         let key_bytes = vec![key; raw_bytes.len()];
-        let xor_bytes = cryptopals_rs::xor::xor(&raw_bytes, &key_bytes).unwrap();
+        let xor_bytes = xor::xor(&raw_bytes, &key_bytes).unwrap();
         let xor_str = String::from_utf8(xor_bytes).unwrap();
         assert_eq!(xor_str, "Cooking MC's like a pound of bacon");
     }
@@ -46,11 +46,12 @@ mod tests {
             let line = line.unwrap();
             let raw_bytes = hex::decode(&line).unwrap();
 
-            let key = cryptopals_rs::xor::break_single_byte_xor(&raw_bytes);
+            let key = xor::break_single_byte_xor(&raw_bytes);
             let key_bytes = vec![key; raw_bytes.len()];
 
-            let xor_bytes = cryptopals_rs::xor::xor(&raw_bytes, &key_bytes).unwrap();
-            let score = cryptopals_rs::score_plaintext::english_score(&xor_bytes);
+            let xor_bytes = xor::xor(&raw_bytes, &key_bytes).unwrap();
+            let score = score_plaintext::english_score(&xor_bytes);
+
             if score > best_score {
                 best_score = score;
                 best_line = match std::str::from_utf8(&xor_bytes) {
@@ -60,5 +61,18 @@ mod tests {
             }
         }
         assert_eq!(best_line, "Now that the party is jumping\n");
+    }
+
+    #[test]
+    fn set_1_challenge_5() {
+        let raw_bytes =
+            b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let key = b"ICE";
+        let expanded_key = xor::expand_key(key, raw_bytes.len());
+
+        let xor_bytes = xor::xor(raw_bytes, &expanded_key).unwrap();
+
+        let xor_str = hex::encode(&xor_bytes);
+        assert_eq!(xor_str, "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
     }
 }
