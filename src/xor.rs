69490c468b342c05517e9060aeb87e73824f2cf1
joaonevess <joaocns0@protonmail.com>
2022-12-03 00:34:39 -0300

    reuse expand_key function

--- src/xor.rs
+++ src/xor.rs
@@ -41,16 +41,11 @@ pub fn break_single_byte_xor(ciphertext: &[u8]) -> (u8, f32, String) {
     (best_key, best_score, best_plaintext)
 }
 
-pub fn repeating_key_xor(input: &[u8], key: &[u8]) -> Result<Vec<u8>, &'static str> {
+pub fn xor_repeating_key(input: &[u8], key: &[u8]) -> Result<Vec<u8>, &'static str> {
     if key.is_empty() {
         return Err("key cannot be empty");
     }
-
-    Ok(input
-        .iter()
-        .zip(key.iter().cycle())
-        .map(|(x, y)| x ^ y)
-        .collect())
+    Ok(xor(input, &expand_key(key, input.len()))?)
 }
 
 #[cfg(test)]
@@ -94,7 +89,7 @@ mod tests {
         let key = b"ICE";
         let expected_output =
             hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap();
-        let output = repeating_key_xor(input, key).unwrap();
+        let output = xor_repeating_key(input, key).unwrap();
         assert_eq!(output, expected_output);
     }
 }
