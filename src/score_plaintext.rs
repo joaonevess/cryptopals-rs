d2c60f1cdcca53dca35d723066c8a45e07d603fd
joaonevess <joaocns0@protonmail.com>
2022-12-03 00:28:42 -0300

    improve score_plaintext

--- src/score_plaintext.rs
+++ src/score_plaintext.rs
@@ -1,52 +1,90 @@
-pub fn english_score(bytes: &[u8]) -> f64 {
-    // Create a fixed-size array to count the frequency of each byte
-    // can be faster than using a HashMap for small inputs
-    let mut byte_counts = [0; 256];
-    for byte in bytes {
-        byte_counts[*byte as usize] += 1;
-    }
+use std::collections::HashMap;
 
-    // Use the English letter frequencies found on Wikipedia
-    // https://en.wikipedia.org/wiki/Letter_frequency#Relative_frequencies_of_letters_in_the_English_language
-    let mut score = 0.0;
-    for (i, count) in byte_counts.iter().enumerate() {
-        // We'll only consider ASCII letters, which have values between 65 and 122 (inclusive)
-        if i >= 65 && i <= 122 {
-            let letter = i as u8 as char;
-            let frequency = match letter {
-                'a' | 'A' => 8.167,
-                'b' | 'B' => 1.492,
-                'c' | 'C' => 2.782,
-                'd' | 'D' => 4.253,
-                'e' | 'E' => 12.702,
-                'f' | 'F' => 2.228,
-                'g' | 'G' => 2.015,
-                'h' | 'H' => 6.094,
-                'i' | 'I' => 6.966,
-                'j' | 'J' => 0.153,
-                'k' | 'K' => 0.772,
-                'l' | 'L' => 4.025,
-                'm' | 'M' => 2.406,
-                'n' | 'N' => 6.749,
-                'o' | 'O' => 7.507,
-                'p' | 'P' => 1.929,
-                'q' | 'Q' => 0.095,
-                'r' | 'R' => 5.987,
-                's' | 'S' => 6.327,
-                't' | 'T' => 9.056,
-                'u' | 'U' => 2.758,
-                'v' | 'V' => 0.978,
-                'w' | 'W' => 2.360,
-                'x' | 'X' => 0.150,
-                'y' | 'Y' => 1.974,
-                'z' | 'Z' => 0.074,
-                _ => 0.0,
-            };
+// https://www.cl.cam.ac.uk/~mgk25/lee-essays.pdf
+fn get_english_frequencies() -> HashMap<u8, f32> {
+    let mut expected_frequencies: HashMap<u8, f32> = HashMap::new();
 
-            score += frequency * (*count as f64);
+    expected_frequencies.insert(b'.', 0.0657); // Other
+    expected_frequencies.insert(b' ', 0.1217);
+    expected_frequencies.insert(b'a', 0.0609);
+    expected_frequencies.insert(b'b', 0.0105);
+    expected_frequencies.insert(b'c', 0.0284);
+    expected_frequencies.insert(b'd', 0.0292);
+    expected_frequencies.insert(b'e', 0.1136);
+    expected_frequencies.insert(b'f', 0.0179);
+    expected_frequencies.insert(b'g', 0.0138);
+    expected_frequencies.insert(b'h', 0.0341);
+    expected_frequencies.insert(b'i', 0.0544);
+    expected_frequencies.insert(b'j', 0.0024);
+    expected_frequencies.insert(b'k', 0.0041);
+    expected_frequencies.insert(b'l', 0.0292);
+    expected_frequencies.insert(b'm', 0.0276);
+    expected_frequencies.insert(b'n', 0.0544);
+    expected_frequencies.insert(b'o', 0.0600);
+    expected_frequencies.insert(b'p', 0.0195);
+    expected_frequencies.insert(b'q', 0.0024);
+    expected_frequencies.insert(b'r', 0.0495);
+    expected_frequencies.insert(b's', 0.0568);
+    expected_frequencies.insert(b't', 0.0803);
+    expected_frequencies.insert(b'u', 0.0243);
+    expected_frequencies.insert(b'v', 0.0097);
+    expected_frequencies.insert(b'w', 0.0138);
+    expected_frequencies.insert(b'x', 0.0024);
+    expected_frequencies.insert(b'y', 0.0130);
+    expected_frequencies.insert(b'z', 0.0003);
+
+    expected_frequencies
+}
+
+fn is_control(c: u8) -> bool {
+    c < 0x20 || c == 0x7f
+}
+
+fn is_alphabetic(c: u8) -> bool {
+    (c >= 0x41 && c <= 0x5A) || (c >= 0x61 && c <= 0x7A)
+}
+
+pub fn get_char_count(input: &[u8]) -> HashMap<u8, f32> {
+    let mut counts: HashMap<u8, f32> = HashMap::new();
+
+    for &c in input {
+        if is_control(c) {
+            continue;
         }
+
+        let key = if is_alphabetic(c) {
+            c.to_ascii_lowercase()
+        } else if c == b' ' || c == b'\t' {
+            b' '
+        } else {
+            b'.'
+        };
+
+        *counts.entry(key).or_insert(0.0) += 1.0;
     }
 
-    // Return the score normalized by the number of bytes in the input slice
-    score / bytes.len() as f64
+    counts
+}
+
+pub fn english_score(bytes: &[u8]) -> f32 {
+    if !bytes.is_ascii() {
+        return f32::MAX;
+    }
+    if bytes.iter().any(|&c| is_control(c) && c != b'\n') {
+        return f32::MAX;
+    }
+
+    let expected_frequencies = get_english_frequencies();
+    let counts = get_char_count(bytes);
+
+    // Calculate the score of the input by summing the squared difference between the
+    // expected and actual frequency of each character.
+    let mut score = 0.0;
+    for (c, count) in counts {
+        let expected = expected_frequencies.get(&c).unwrap_or(&0.0);
+        let actual = count / bytes.len() as f32;
+        score += (expected - actual).powi(2);
+    }
+
+    score
 }
