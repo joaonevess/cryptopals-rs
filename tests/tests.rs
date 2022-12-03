3be3faddefcca90002b59c602cf131f6a264a292
joaonevess <joaocns0@protonmail.com>
2022-12-03 00:29:19 -0300

    fix tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -21,43 +21,32 @@ mod tests {
         assert_eq!(xor_str, "746865206b696420646f6e277420706c6179");
     }
 
-    // write a test that breaks single XOR cipher
     #[test]
     fn set_1_challenge_3() {
         let raw_bytes =
             hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
                 .unwrap();
 
-        let key = xor::break_single_byte_xor(&raw_bytes);
-        let key_bytes = vec![key; raw_bytes.len()];
-        let xor_bytes = xor::xor(&raw_bytes, &key_bytes).unwrap();
-        let xor_str = String::from_utf8(xor_bytes).unwrap();
-        assert_eq!(xor_str, "Cooking MC's like a pound of bacon");
+        let (_, _, result) = xor::break_single_byte_xor(&raw_bytes);
+        assert_eq!(result, "Cooking MC's like a pound of bacon");
     }
 
     #[test]
     fn set_1_challenge_4() {
         let file = std::fs::File::open("tests/challengedata/4.txt").unwrap();
         let reader = std::io::BufReader::new(file);
-        let mut best_score = 0.0;
+        let mut best_score = f32::MAX;
         let mut best_line = String::new();
 
         for line in std::io::BufRead::lines(reader) {
             let line = line.unwrap();
             let raw_bytes = hex::decode(&line).unwrap();
 
-            let key = xor::break_single_byte_xor(&raw_bytes);
-            let key_bytes = vec![key; raw_bytes.len()];
+            let (_, score, decrypted) = xor::break_single_byte_xor(&raw_bytes);
 
-            let xor_bytes = xor::xor(&raw_bytes, &key_bytes).unwrap();
-            let score = score_plaintext::english_score(&xor_bytes);
-
-            if score > best_score {
+            if score < best_score {
                 best_score = score;
-                best_line = match std::str::from_utf8(&xor_bytes) {
-                    Ok(v) => v.to_string(),
-                    Err(e) => format!("Invalid UTF-8 sequence: {}", e),
-                };
+                best_line = decrypted;
             }
         }
         assert_eq!(best_line, "Now that the party is jumping\n");
